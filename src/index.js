9c586f0b299f3d717722db6b197c81eabdc38aad
Farmata Kane <kane.farmata@gmail.com>
2025-08-19 20:09:09 +0100

    Finalise basic navigation logic

--- src/index.js
+++ src/index.js
@@ -1,48 +1,58 @@
 // the importer (where you IN/import the modules, there is only 1)
 
 import "./styles.css";
-import { menuGreeting } from "./menu.js"
+import { menuGreeting } from "./menu.js";
 //import "./about"
 export const aboutUsBtn = document.createElement("button")
 const mainDiv = document.querySelector("#content");
 
 
 const homeBtn = document.querySelector("#home-btn");
-const homeTitle = document.createElement("h1")
+const homeTitle = document.createElement("h1");
 
 const aboutUsTitle = document.createElement("h1");
-const aboutUsPara = document.createElement("p")
-import { aboutUsText } from "./asset/aboutUsText.js"
+const aboutUsPara = document.createElement("p");
+import { aboutUsText } from "./asset/aboutUsText.js";
 
-const menuBtn = document.querySelector("#menu-btn")
+const menuBtn = document.querySelector("#menu-btn");
 const menuTitle = document.createElement("h1");
 const menuDiv = document.createElement("div");
-import smallImg from "./asset/300x300.png"
+import smallImg from "./asset/300x300.png";
+
+const contactBtn = document.querySelector("#contact-btn");
+const contactTitle = document.createElement("h1");
+const contactDiv = document.createElement("div");
+const contactNumber = document.createElement("p");
+const mapLocation = document.createElement("iframe");
 
 homeBtn.addEventListener("click", () => {
-  displayHomePage()
+  loadHomePage()
 });
 
 aboutUsBtn.addEventListener("click", () => {
-  displayAboutUsPage()
+  loadAboutUsPage()
 });
 
 menuBtn.addEventListener("click", () => {
-  displayMenuPage()
+  loadMenuPage()
+});
+
+contactBtn.addEventListener("click", ()=> {
+  loadContactPage()
 })
 
-displayHomePage()
+loadHomePage();
 
-function displayHomePage() {
+function loadHomePage() {
   homeTitle.textContent = "Welcome to Fratelli!";
   aboutUsBtn.textContent = "About";
 
   mainDiv.innerHTML = ""
   mainDiv.appendChild(homeTitle)
   mainDiv.appendChild(aboutUsBtn)
-}
+};
 
-function displayAboutUsPage() {
+function loadAboutUsPage() {
   aboutUsTitle.textContent = "About Us"
   aboutUsPara.textContent = aboutUsText
 
@@ -50,27 +60,27 @@ function displayAboutUsPage() {
   mainDiv.appendChild(aboutUsTitle)
   mainDiv.appendChild(aboutUsPara)
 
-}
+};
 
-function displayMenuPage() { 
+function loadMenuPage() { 
   menuTitle.textContent = "Our Menu";
 
   mainDiv.innerHTML = "";
     mainDiv.appendChild(menuTitle);
   mainDiv.appendChild(menuDiv)
 
-  displayItems()
+  loadItems()
 
 
-}
+};
 
-function displayItems() {
+function loadItems() {
 
 
   for (let i = 0; i < 10; i++) {
     const menuItem = document.createElement("section");
-    const menuItemTitle = document.createElement("h3")
-    const menuItemPrice = document.createElement("span")
+    const menuItemTitle = document.createElement("h3");
+    const menuItemPrice = document.createElement("span");
     
     menuItem.classList.add("menu-item-section")
     menuItem.style.backgroundImage = `url(${smallImg})`;
@@ -82,6 +92,19 @@ function displayItems() {
     menuItem.appendChild(menuItemPrice)
   }
 
-}
+};
 
-console.log(menuGreeting)
+function loadContactPage() {
+  contactNumber.textContent = "Phone number: 0123846292";
+  mapLocation.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2430.373791753437!2d-1.8648494!3d52.4723675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bbe2ab363cd1%3A0x180f10231a78f6d1!2sFRATELLI!5e0!3m2!1sen!2sma!4v1755552285746!5m2!1sen!2sma"
+  mapLocation.classList.add("google-maps-style")
+
+  mainDiv.innerHTML = "";
+  mainDiv.appendChild(contactTitle)
+  mainDiv.appendChild(contactDiv);
+  contactDiv.appendChild(contactNumber)
+  contactDiv.appendChild(mapLocation)
+
+};
+
+console.log(menuGreeting);
